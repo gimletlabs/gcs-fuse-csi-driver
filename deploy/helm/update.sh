1f39351618ba05be96f20f3108527b4cbe40cc25
James Bartlett <james@gimletlabs.ai>
2024-02-05 21:33:09 -0800

    [GML] Fixes to get driver to work
    Signed-off-by: James Bartlett <james@gimletlabs.ai>

--- deploy/helm/update.sh
+++ deploy/helm/update.sh
@@ -1,5 +1,6 @@
-make generate-spec-yaml REGISTRY="GML_IMAGEREGISTRY" STAGINGVERSION="GML_VERSION" PROJECT="GML_PROJECT"
+make generate-spec-yaml REGISTRY="GML_IMAGEREGISTRY" STAGINGVERSION="GML_VERSION" PROJECT="GML_PROJECT" CA_BUNDLE="GML_CA_BUNDLE"
 
 sed 's|GML_IMAGEREGISTRY|{{.Values.imageregistry}}|g' bin/gcs-fuse-csi-driver-specs-generated.yaml | \
-  sed 's|GML_VERSION|{{.Values.version}}|g' | \
+  sed 's|GML_VERSION|{{.Values.imageversion}}|g' | \
+  sed 's|GML_CA_BUNDLE|{{.Values.caCert}}|g' | \
   sed 's|GML_PROJECT|{{.Values.project}}|g' > deploy/helm/templates/all.yaml
